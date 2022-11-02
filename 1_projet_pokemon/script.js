4820f53fd1fd2c986e820755bb6de20002528c13
XavierC-13 <113617551+XavierC-13@users.noreply.github.com>
2022-11-02 13:16:17 -0400

    comment box

--- 1_projet_pokemon/script.js
+++ 1_projet_pokemon/script.js
@@ -145,3 +145,29 @@ document.getElementById("question").innerHTML = myQuestion1.show();
 
 //3) Instancier les objets nécessaires à votre code qui à partir des classes et sous-classe
 //correspondantes. xavier
+
+$(document).ready(function(){ 
+  
+  $(".primaryContained").on('click', function(){
+  $(".comment").addClass("commentClicked");
+});//end click
+$("textarea").on('keyup.enter', function(){
+  $(".comment").addClass("commentClicked");
+});//end keyup
+});//End Function
+
+new Vue({
+  el: "#app",
+  data:{
+     title: 'Add a comment',
+    newItem: '',
+    item: [],
+  },
+  methods:{
+    addItem  (){
+    this.item.push(this.newItem);
+      this.newItem = "";
+    }
+}
+
+});
